4e19d1b1c5b45c7635d04c637f5fbe3db32fa23b
AnandZam <anand@gochoo.in>
2025-04-23 22:34:53 +0530

    first commit

---
+++ Josh.js
@@ -0,0 +1,61 @@
+function toggleVideo() {
+    const video = document.getElementById("myVideo");
+    const playIcon = document.getElementById("playIcon");
+  
+    if (video.paused) {
+      video.play();
+      playIcon.style.display = "none";
+    } else {
+      video.pause();
+      playIcon.style.display = "block";
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.getElementById('slides');
+    const dots = document.querySelectorAll('.dot');
+    let currentSlide = 0;
+    let totalSlides = dots.length;
+    let interval;
+
+    function goToSlide(index) {
+      currentSlide = index % totalSlides;
+      slides.style.transform = `translateX(-${currentSlide * 100}vw)`;
+      dots.forEach(dot => dot.classList.remove('active'));
+      dots[currentSlide].classList.add('active');
+    }
+
+    function nextSlide() {
+      goToSlide(currentSlide + 1);
+    }
+
+    function startSlider() {
+      interval = setInterval(nextSlide, 5000); // Change every 5 seconds
+    }
+
+    // Dots click event
+    dots.forEach((dot, idx) => {
+      dot.addEventListener('click', () => {
+        clearInterval(interval);
+        goToSlide(idx);
+        startSlider();
+      });
+    });
+
+    goToSlide(0);
+    startSlider();
+  });
+
+
+
+ const open = document.getElementById("open");
+ const modalContainer = document.getElementById("modal-container"); 
+ const close = document.getElementById("close");
+ 
+ open.addEventListener('click', () => {
+  modalContainer.classList.add('show');
+ });
+ close.addEventListener('click', () => {
+  modalContainer.classList.remove('show');
+ });
+
